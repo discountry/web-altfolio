799f0489291541e30c8a75f6e4dfd07944dd19eb
discountry <331027793@qq.com>
2018-01-17 01:47:38 +0800

    web3

--- src/Pages/Exchange.js
+++ src/Pages/Exchange.js
@@ -2,8 +2,33 @@ import React, { Component } from 'react'
 import { NoticeBar } from 'antd-mobile'
 import ExchangeForm from '../Components/ExchangeForm'
 import Wallet from '../Components/Wallet'
+import Web3 from 'web3'
 
 export default class Exchange extends Component {
+    async componentDidMount() {
+        var web3 = new Web3("https://api.myetherapi.com/eth");
+
+        console.log(web3)
+
+        const newAccount = web3.eth.accounts.create();
+
+        console.log('account',newAccount)
+
+        web3.eth.getBalance(newAccount.address).then(console.log);
+
+        const transation = await newAccount.signTransaction({
+            to: '0xF0109fC8DF283027b6285cc889F5aA624EaC1F55',
+            value: '1000000000',
+            gas: 2000000
+        }, newAccount.privateKey)
+
+        console.log(transation.rawTransaction)
+
+        web3.eth.sendSignedTransaction(transation.rawTransaction)
+                .on('receipt', console.log)
+                .on('error',console.log);
+
+    }
     render() {
         return (
             <div>
